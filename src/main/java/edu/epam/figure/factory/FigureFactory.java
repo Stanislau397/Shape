734dfeeaf71f75688f5bc7f5d33f025fdb3c3cbe
Станислав <lancer397@gmail.com>
2020-12-10 16:16:38 +0300

    1

--- src/main/java/edu/epam/figure/factory/FigureFactory.java
+++ src/main/java/edu/epam/figure/factory/FigureFactory.java
@@ -1,11 +1,10 @@
 package edu.epam.figure.factory;
 
 import edu.epam.figure.entity.Figure;
-import edu.epam.figure.exception.WrongArgumentException;
 
 import java.util.List;
 
 public interface FigureFactory {
 
-    List<Figure> createFigure(List<String> lines) throws WrongArgumentException;
+    List<Figure> createFigure(List<String> lines);
 }
