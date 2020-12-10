313acec535e3605a0a1b5c83124d5900245075b8
Станислав <lancer397@gmail.com>
2020-12-10 16:10:06 +0300

    1

--- src/main/java/edu/epam/figure/validation/EllipseValidator.java
+++ src/main/java/edu/epam/figure/validation/EllipseValidator.java
@@ -9,16 +9,16 @@ import java.util.List;
 public class EllipseValidator {
 
     public static final Logger logger = LogManager.getLogger(EllipseValidator.class);
-    public static final int NUMBER_ELLIPSE_ARGUMENTS = 4;
-    public static final int UPPER_LEFT_COORDINATE_X = 0;
-    public static final int UPPER_LEFT_COORDINATE_Y = 1;
-    public static final int LOWER_RIGHT_COORDINATE_X = 2;
-    public static final int LOWER_RIGHT_COORDINATE_Y = 3;
+    public static final int ELLIPSE_ARGUMENTS = 4;
+    public static final int UPPER_LEFT_X = 0;
+    public static final int UPPER_LEFT_Y = 1;
+    public static final int LOWER_RIGHT_X = 2;
+    public static final int LOWER_RIGHT_Y = 3;
 
     public boolean isValidEllipse(List<Double> coordinates) {
-        boolean result = coordinates.size() == NUMBER_ELLIPSE_ARGUMENTS
-                && !(coordinates.get(UPPER_LEFT_COORDINATE_X).equals(coordinates.get(LOWER_RIGHT_COORDINATE_X))
-                || coordinates.get(UPPER_LEFT_COORDINATE_Y).equals(coordinates.get(LOWER_RIGHT_COORDINATE_Y)));
+        boolean result = coordinates.size() == ELLIPSE_ARGUMENTS
+                && !(coordinates.get(UPPER_LEFT_X).equals(coordinates.get(LOWER_RIGHT_X))
+                || coordinates.get(UPPER_LEFT_Y).equals(coordinates.get(LOWER_RIGHT_Y)));
 
         if (!result) {
             logger.log(Level.DEBUG, "Invalid Data");
